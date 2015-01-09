e772b839e1fa3a8e260b9a09ae5c8c7791a775b0
Thomas Babu <tommy.developer@gmail.com>
2015-01-09 17:03:44 +0800

    added bearer authentication to manage oath2 request

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,8 @@ Auth Controller to manage authentication
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user.js');
+var BearerStrategy = require('password-http-bearer').Strategy;
+var Token = require('../models/token');
 
 passport.use(new BasicStrategy(
   function(username, password, callback) {
@@ -33,4 +35,30 @@ passport.use(new BasicStrategy(
   }
 ));
 
+passport.use(new BearerStrategy(
+  function(accessToken, callback) {
+    Token.findOne({ value : accessToken }, function(err, token) {
+      if (err)
+          return callback(err);
+
+      // No token found
+      if(!token)
+        return callback(null, false);
+
+      User.findOne({ _id { token.userId }, function(err, user) {
+        if( err )
+          return callback(err);
+
+        // No user found
+        if( !user )
+          return callback(null, false);
+
+        // Simple example with no scope
+        callback( null, user , { scope : '*'});
+      });
+    });
+  }
+));
+
 exports.isAuthenticated = passport.authenticate('basic', { session : false} );
+exports.isBearerAuthenticated = passport.authenticate('bearer', { session : false });
