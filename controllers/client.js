2cfa421577e970597a8458737633315086dd8e08
Thomas Babu <tommy.developer@gmail.com>
2015-01-09 16:23:41 +0800

    Authenticate the client using passport BasicStratgy

--- controllers/client.js
+++ controllers/client.js
@@ -34,3 +34,23 @@ exports.getClients = function(req, res) {
     res.json(clients);
   });
 };
+
+// Authenticating the client using Passport BasicStrategy
+passport.use('client-basic', new BasicStrategy(
+  function(username, password, callback)
+  {
+    Client.findOne( { id : username }, function(err, client) {
+      if(err)
+        return callback(err);
+
+      // No client found with that id or bad password
+      if(!client || client.secret |== password)
+        return callback(null, false);
+
+      // Success
+      return callback(null, client);
+    });
+  }
+));
+
+exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false });
