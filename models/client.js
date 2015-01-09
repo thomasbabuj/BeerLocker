491217df5917a8cd4dfb69975421c27508be8e45
Thomas Babu <tommy.developer@gmail.com>
2015-01-09 13:08:33 +0800

    created client schema in a model

---
+++ models/client.js
@@ -0,0 +1,17 @@
+/*
+ This is to manage application clients
+ */
+
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our client schema
+var clientSchema = new mongoose.Schema ({
+  name : { type : String, unique : true, required : true },
+  id : { type : String, required: true },
+  secret : { type : String , required : true },
+  userId : { type : String, required : true }
+});
+
+// Export the Mongoose model
+module.exports = mongoose.mode('Client', ClientSchema);
