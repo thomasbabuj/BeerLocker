b0a748e938f401502872559f576a94e37c152c7b
Thomas Babu <tommy.developer@gmail.com>
2015-01-09 13:01:31 +0800

    locked down API endpoints and only allowing access to a user's own beer locker

--- controllers/beer.js
+++ controllers/beer.js
@@ -14,6 +14,7 @@ exports.postBeers = function(req, res) {
   beer.name = req.body.name;
   beer.type = req.body.type;
   beer.quantity = req.body.quantity;
+  beer.userId = req.user._id;
 
   // Save the beer and check for errors
   beer.save(function(err){
@@ -27,7 +28,8 @@ exports.postBeers = function(req, res) {
 // Create endpoint /api/beers for GET
 exports.getBeers = function(req, res){
     // Use the Beer model to find all beer
-    Beer.find(function(err, beers){
+    // Get all the beers added by the user
+    Beer.find( { userId: req.user._id } , function(err, beers){
         if( err )
             res.send(err);
 
@@ -37,8 +39,8 @@ exports.getBeers = function(req, res){
 
 // Create endpoint /beers/:beer_id prefix for GET
 exports.getBeer = function(req, res) {
-    // Use the Beer model to find a specific beer
-    Beer.findById(req.params.beer_id, function(err, beer) {
+    // Use the Beer model to find a specific beer of the user
+    Beer.find({ userId : req.user._id , _id : req.params.beer_id }, function(err, beer) {
         if(err)
             res.send(err);
 
@@ -49,30 +51,21 @@ exports.getBeer = function(req, res) {
 // Create a endpoint /api/beers/:beer_id for PUT
 exports.putBeer = function(req, res){
     // Use the Beer model to find a specific beer
-    Beer.findById(req.params.beer_id, function(err, beer){
-        if(err)
-            res.send(err);
+    Beer.update({ userId : req.user._id , _id : req.params.beer_id }, { quantity : req.body.quantity }, function(err, num, raw ) {
+      if (err)
+        res.send( err );
 
-        // Update the existing beer quantity
-        beer.quantity = req.body.quantity;
-
-        // Save the beer and check for errors
-        beer.save(function(err){
-            if (err)
-                res.send(err);
-
-            res.json(beer);
-        });
+      res.json({ message : num + 'updated' });
     });
 };
 
 // Create endpoint /api/beers/:beer_id for DELETE
 exports.deleteBeer = function(req, res){
     // Use the Beer model to find a specific beer and remove it
-    Beer.findByIdAndRemove(req.params.beer_id, function(err, beer) {
-        if (err)
-            res.send(err);
+    Beer.remove({ userId : req.user._id, _id : req.params.beer_id }, function(err) {
+      if (err)
+          res.send(err);
 
-        res.json({ message : 'Beer removed from the locker!'});
+      res.json({ message : 'Beer removed from the locker!' });
     });
 };
